7c57fc5ee84764aef71e1a1c12cee1af74835fb4
sterligova <id.sterligova@gmail.com>
2024-02-16 17:16:01 +0100

    feat transform

--- src/insight_pipeline_package/transform.py
+++ src/insight_pipeline_package/transform.py
@@ -1,5 +1,21 @@
-def process_data(raw_data):
-    # process data here
-    process_data = raw_data
-    
-    return process_data
+from pyspark.sql.functions import col, when
+
+
+def process_data(raw_data, spark):
+    # Remove duplicates
+    processed_data = raw_data.dropDuplicates()
+
+    # Replace empty values with Null
+    for column in processed_data.columns:
+        processed_data = processed_data.withColumn(column, \
+            when(col(column) == "", None).otherwise(col(column)))
+
+    # Drop rows with mostly Null values
+    # processed_data = processed_data.na.drop(how='any')
+        
+    # Drop rows with more than 50% null values
+    #processed_data = processed_data.na.drop(thresh=len(processed_data.columns) / 2)
+
+    return processed_data
+
+
